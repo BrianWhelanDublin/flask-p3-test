02604b4dc85d0fbd25cba2a64da89d8a8e2684ab
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-01-07 15:50:01 +0000

    add register errors

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ from flask import (Flask, flash, render_template,
                    url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-from forms import RegistrationForm, LoginForm
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import (DataRequired,
+                                Length, Email,
+                                EqualTo,
+                                ValidationError)
+from flask_bcrypt import Bcrypt
 if os.path.exists("env.py"):
     import env
 
@@ -18,6 +24,43 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 
 mongo = PyMongo(app)
+bcrypt = Bcrypt(app)
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Username",
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField("Email",
+                        validators=[DataRequired(), Email()])
+    password = PasswordField("Password",
+                             validators=[DataRequired()])
+    confirm_password = PasswordField("Confirm Password",
+                                     validators=[DataRequired(),
+                                                 EqualTo("password")])
+    submit = SubmitField("Sign Up")
+
+    def validate_username(self, username):
+        user = mongo.db.users.find_one(
+            {"username": username.data.lower()}
+        )
+        if user:
+            raise ValidationError("Username already exists")
+
+    def validate_email(self, email):
+        user = mongo.db.users.find_one(
+             {"email": email.data}
+        )
+        if user:
+            raise ValidationError("Email already exists")
+
+
+class LoginForm(FlaskForm):
+    username = StringField("Username",
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField("Password",
+                             validators=[DataRequired()])
+    remember = BooleanField("Remember Me")
+    submit = SubmitField("Login")
 
 
 @app.route("/")
@@ -34,10 +77,27 @@ def get_posts():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+
     form = RegistrationForm()
+
     if form.validate_on_submit():
-        flash(f"Account created for {form.username.data}!", "success")
-        return redirect(url_for("home"))
+
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data).decode("utf-8")
+
+        register_user = {
+            "username": form.username.data.lower(),
+            "email": form.email.data,
+            "password": hashed_password,
+            "posts": ""
+        }
+
+        mongo.db.users.insert_one(register_user)
+
+        flash("Account created You may now login", "success")
+
+        return redirect(url_for("login"))
+
     return render_template("register.html",
                            title="Register", form=form)
 
--- forms.py
+++ forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import (DataRequired,
+                                Length, Email,
+                                EqualTo,
+                                ValidationError)
 
 
 class RegistrationForm(FlaskForm):
@@ -14,6 +17,16 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(),
                                                  EqualTo("password")])
     submit = SubmitField("Sign Up")
+
+    def validate_username(self, username):
+        user = mongo.db.users.find_one(username.data)
+        if user:
+            raise ValidationError("Username already exists")
+
+    def validate_email(self, email):
+        user = mongo.db.users.find_one(email.data)
+        if user:
+            raise ValidationError("Email already exists")
 
 
 class LoginForm(FlaskForm):
